1cf1c172837383decc8eb45940cb91146a4d11f8
Alexei <alexei.prudnikov@gmail.com>
2022-06-07 22:38:40 +0300

    Add task04

--- Task04.py
+++ Task04.py
@@ -1,17 +1,40 @@
+def RLEcoding(text):
+    stat = []
+    for i in text:
+        if (len(stat) == 0):
+            stat.append([1, i])
+        else:
+            if stat[len(stat) - 1][1] == i:
+                stat[len(stat) - 1][0] += 1
+            else:
+                stat.append([1, i])
+    rleResult = ''
+    for i in stat:
+        rleResult += ''.join(map(str, i))
+    return rleResult
+def RLEencoding(text):
+    count = ''
+    result = ''
+    for i in text:
+        if 48 <= ord(i) <= 57:
+            count += i
+        else:
+            for z in range(int(count)):
+                result += i
+            count = ''
+    return result
 file = open('text.txt', 'r')
 line = file.read()
-stat = []
-for i in line:
-    if (len(stat) == 0):
-        stat.append([1, i])
-    else:
-        if stat[len(stat) - 1][1] == i:
-            stat[len(stat) - 1][0] += 1
-        else:
-            stat.append([1, i])
 file.close()
-zipResult = ''
-for i in stat:
-    zipResult += ''.join(map(str, i))
-print(zipResult)
+coding = RLEcoding(line)
+file = open('rle_coding.txt', 'w')
+file.write(coding)
+file.close()
+file = open('rle_coding.txt', 'r')
+code = file.read()
+file.close()
+file = open('rle_encoding.txt', 'w')
+file.write(RLEencoding(code))
+file.close()
+
 
